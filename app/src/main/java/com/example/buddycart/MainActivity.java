ff8387aec8e243ebe38c5bc107d61eb678f8e179
megan <davamae.md@gmail.com>
2025-03-24 17:10:35 -0700

    Added ActivityMain2, functionality for continue and continue-google button, added favorites, history, order buttons, and sale banner for ActivityMain2.

--- app/src/main/java/com/example/buddycart/MainActivity.java
+++ app/src/main/java/com/example/buddycart/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.buddycart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        //references for buttons
+        Button continueButton = findViewById(R.id.continueButton);
+        Button googleButton = findViewById(R.id.googleButton);
+
+        //onClickListener for continue button
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //open second activity screen when continue button clicked
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+        //set onClickListener for continue with Google button
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //begin mainActivity2 when google button clicked
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
