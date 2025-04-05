97203b19c5250e26d535b85b2789994839afb460
megan <davamae.md@gmail.com>
2025-04-05 15:58:18 -0700

    fixed button colours, added functionality to LoginPhoneNumberActivity to navigate to LoginCodeActivity and displaying a toast msg of the number entered

---
+++ app/src/main/java/com/example/buddycart/LoginCodeActivity.java
@@ -0,0 +1,40 @@
+package com.example.buddycart;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginCodeActivity extends AppCompatActivity {
+
+    String phoneNumber;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login_code);
+
+
+        //getting phone number from intent
+        phoneNumber = getIntent().getStringExtra("phone");
+
+        //display phone number in toast
+        if(phoneNumber != null) {
+            Toast.makeText(getApplicationContext(), phoneNumber, Toast.LENGTH_SHORT).show();
+        }
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
