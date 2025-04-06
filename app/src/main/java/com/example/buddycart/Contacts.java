33ac07cfb98cb0146134f26488f2bec26e93515f
megan <davamae.md@gmail.com>
2025-04-06 11:08:33 -0700

    made home button in Contacts to navigate back to MainActivity2.

--- app/src/main/java/com/example/buddycart/Contacts.java
+++ app/src/main/java/com/example/buddycart/Contacts.java
@@ -49,8 +49,9 @@ public class Contacts extends AppCompatActivity {
                 if(item.getItemId() == R.id.menu_chat){
                     getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout, chatFragment).commit();
                 }
-                if(item.getItemId() == R.id.menu_profile){
-                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout, profileFragment).commit();
+                if(item.getItemId() == R.id.menu_home){
+                    Intent intent2 = new Intent(Contacts.this, MainActivity2.class);
+                    startActivity(intent2);
                 }
                 if(item.getItemId() == R.id.menu_add){
                     Intent intent = new Intent(Contacts.this, LoginPhoneNumberActivity.class);
