e8aec24c743ccb25a37e3af80110e7718e68f4aa
megan <davamae.md@gmail.com>
2025-04-05 20:40:47 -0700

    changed Contacts screen, added a searchbar button, chat, profile, and add button. also included navigation to the contacts screen after name is entered.

--- app/src/main/java/com/example/buddycart/LoginUsernameActivity.java
+++ app/src/main/java/com/example/buddycart/LoginUsernameActivity.java
@@ -1,6 +1,11 @@
 package com.example.buddycart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,51 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginUsernameActivity extends AppCompatActivity {
 
+    EditText nameInput;
+    Button nextBtn;
+    ProgressBar progressBar;
+    String phoneNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_username);
+
+        nameInput = findViewById(R.id.login_name);
+        nextBtn = findViewById(R.id.login_next_btn);
+        progressBar = findViewById(R.id.login_progress_bar);
+
+        //get phonenumber from intent
+        phoneNumber = getIntent().getStringExtra("phone");
+
+        //setup next button to navigate to LoginUsernameActivity
+        nextBtn.setOnClickListener(v ->{
+            //start LoginUsernameActivity when next btn pressed
+            Intent intent = new Intent(LoginUsernameActivity.this, Contacts.class);
+            startActivity(intent);
+        });
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+    }
+
+
+
+    void setInProgress(boolean inProgress){
+        if(inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            nextBtn.setVisibility(View.GONE);
+        } else{
+            progressBar.setVisibility(View.GONE);
+            nextBtn.setVisibility(View.VISIBLE);
+        }
     }
 }
---
+++ app/src/main/java/com/example/buddycart/utils/FirebaseUtil.java
@@ -0,0 +1,9 @@
+package com.example.buddycart.utils;
+
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class FirebaseUtil {
+
+
+}
