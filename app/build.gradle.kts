298847b9bd4e2715dd450025e87a82842cc29cab
Aakash Singh <aakashds@student.ubc.ca>
2025-04-06 17:53:11 -0700

    fixed maps issue.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.google.gms.google.services)
-
-
-
+    // Remove the secrets plugin since we're not using it
 }
 
 android {
@@ -16,8 +14,12 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        // Read the API key from local.properties. This property isn't a Gradle extra by default,
+        // so we use 'findProperty' to try to get it.
+        val mapsApiKey = project.findProperty("MAPS_API_KEY") as? String ?: "YOUR_DEFAULT_API_KEY"
+        manifestPlaceholders["MAPS_API_KEY"] = mapsApiKey
     }
 
     buildTypes {
@@ -38,31 +40,8 @@ android {
     }
 }
 
-
-//Adds a plugin that helps with implementing the map
-buildscript {
-    dependencies {
-        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
-    }
-}
-
-secrets {
-    // To add Maps API key to this project:
-    //        MAPS_API_KEY=YOUR_API_KEY
-    propertiesFileName = "secrets.properties"
-
-    // A properties file containing default secret values. This file can be
-    // checked in version control.
-    defaultPropertiesFileName = "local.defaults.properties"
-}
-
-
-
 dependencies {
-
-
-    implementation("com.google.android.gms:play-services-maps:19.0.0")
-
+    implementation("com.google.android.gms:play-services-maps:19.1.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -71,7 +50,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-
-
-
 }
