1ec3837c82322ad45ed69e4a561e0b65e1a909bf
Gabriel <02awsome18@gmail.com>
2025-04-07 15:05:54 -0700

    Gabe - Backend of Ecommerce nearing completion, begining work on checkout soon

---
+++ app/src/main/java/com/example/buddycart/shopConfirm.java
@@ -0,0 +1,126 @@
+package com.example.buddycart;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class shopConfirm extends AppCompatActivity {
+
+    //counter
+    private static int counter;
+
+    //features
+    private ImageButton add;
+    private ImageButton sub;
+    private Button confirm;
+    private Button cancel;
+
+    //setup features
+    private TextView wantToBuyX;
+    private ImageView showBuy;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_shop_confirm);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //set up on instantiation
+
+        wantToBuyX = findViewById(R.id.addToCart);
+        wantToBuyX.setText("Add " + shoppingPage.itemName + " to cart?"); //sets top of page text to say item name
+
+        showBuy = findViewById(R.id.showItemImg);
+        showBuy.setImageDrawable(shoppingPage.imagePlace);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // do activities
+        counter = 1;
+        add = findViewById(R.id.plusBtn);
+        sub = findViewById(R.id.minusBtn);
+        confirm = findViewById(R.id.confirmBtn);
+        cancel = findViewById(R.id.cancelBtn);
+
+        //add and subtraction commands
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+            }
+        });
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (counter > 1) {
+                    counter--;
+                }
+            }
+        });
+        //Goes back to previous screen
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        //confirm and add to list
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //makes a new spot in all the arrays, new index (i) is the information for that item in the cart
+                shoppingPage.cartContents.add(shoppingPage.itemName);
+                shoppingPage.ammountInCart.add(counter);
+                shoppingPage.costItem.add(shoppingPage.cost);
+
+                finish();
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/buddycart/shoppingPage.java
+++ app/src/main/java/com/example/buddycart/shoppingPage.java
@@ -1,6 +1,12 @@
 package com.example.buddycart;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +15,33 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
+import java.util.BitSet;
 
 public class shoppingPage extends AppCompatActivity {
 
-    public ArrayList<String> cartContents;
-    public ArrayList<Integer> ammountInCart;
+    //Data Collection
+    public static ArrayList<String> cartContents;// array of strings naming each item in the cart
+    public static ArrayList<Integer> ammountInCart;//array of ints telling how much of each item is in cart
+    public static ArrayList<Double> costItem;//array of item costs per item
 
+    //Selected item data
+    public static String itemName;//name of selected item
+    public static double cost; //maluable cost of item
+    public static Drawable imagePlace;//store the src of an image
+
+    //Page Features
+    private ImageButton apple;
+    private ImageButton banana;
+    private ImageButton eggs;
+    private ImageButton honey;
+    private ImageButton soap;
+    private ImageButton shirt;
+    private ImageButton watermellon;
+    private Button fav;
+    private Button history;
+    private Button sales;
+    private Button seeMore1;
+    private Button seeMore2;
 
 
     @Override
@@ -26,6 +53,78 @@ public class shoppingPage extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        //Handles when a user presses one of the shopping items
+        apple = findViewById(R.id.appleBtn);
+        apple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagePlace = apple.getDrawable();
+                cost = 0.99;
+                itemName = "Apple";
+                sendToConfirmPage();
+            }
+        });
+        banana = findViewById(R.id.banBtn);
+        banana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagePlace = banana.getDrawable();
+                cost = 1.35;
+                itemName = "Banana";
+                sendToConfirmPage();
+            }
+        });
+        eggs = findViewById(R.id.eggBtn);
+        eggs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagePlace = eggs.getDrawable();
+                cost = 4.17;
+                itemName = "Eggs";
+                sendToConfirmPage();
+            }
+        });
+        honey = findViewById(R.id.honBtn);
+        honey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagePlace = honey.getDrawable();
+                cost = 6.77;
+                itemName = "Honey";
+                sendToConfirmPage();
+            }
+        });
+        soap = findViewById(R.id.soapBtn);
+        soap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagePlace = soap.getDrawable();
+                cost = 11.78;
+                itemName = "Soap";
+                sendToConfirmPage();
+            }
+        });
+        shirt = findViewById(R.id.shiBtn);
+        shirt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagePlace = shirt.getDrawable();
+                cost = 27.99;
+                itemName = "Shirt";
+                sendToConfirmPage();
+            }
+        });
+        watermellon = findViewById(R.id.watBtn);
+        watermellon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagePlace = watermellon.getDrawable();
+                cost = 5.97;
+                itemName = "Watermellon";
+                sendToConfirmPage();
+            }
         });
 
 
@@ -40,7 +139,22 @@ public class shoppingPage extends AppCompatActivity {
 
 
 
-
-
     }
+
+
+
+
+
+
+// Whenever an image button is selected it loads its information to the relative fields and then calls this method to go to the confirm screen
+    private void sendToConfirmPage (){
+        Intent intent = new Intent(shoppingPage.this, shopConfirm.class);
+        startActivity(intent);
+    }
+
+
+
+
+
+
 }
