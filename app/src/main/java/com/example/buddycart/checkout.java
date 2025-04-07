a094f9fdd6d148fb0bd88429880d35902b20ff24
Gabriel <02awsome18@gmail.com>
2025-04-07 13:25:50 -0700

    Gabe - Frontend of ecommerce finished, begining checkout page

--- app/src/main/java/com/example/buddycart/checkout.java
+++ app/src/main/java/com/example/buddycart/checkout.java
@@ -8,39 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.ArrayList;
-
-public class shoppingPage extends AppCompatActivity {
-
-    public ArrayList<String> cartContents;
-    public ArrayList<Integer> ammountInCart;
-
-
+public class checkout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_shopping_page);
+        setContentView(R.layout.activity_checkout);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
